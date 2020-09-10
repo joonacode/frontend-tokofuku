d7b167748b36628556c0dd56916947ea9e666d41
Deni Irawan Nugraha <60280044+deni-irawan40563@users.noreply.github.com>
2020-09-10 08:00:09 +0700

    add product and mybag page

---
+++ src/views/Products.vue
@@ -0,0 +1,324 @@
+<template>
+<div class="container">
+        <div class="row">
+            <div class="mt-4 col-12">
+                <carousel>
+                        <b-img src="https://id-test-11.slatic.net/p/4f35f5774bc887b4a984ff9b89dfcfee.jpg" fluid alt="Fluid image" class="banner"></b-img>
+                        <b-img src="https://folkswedding.com/js/fileman/Uploads/prewed%20background%20polos/201.jpg" fluid alt="Fluid image" class="banner"></b-img>
+                        <b-img src="https://cdn-image.hipwee.com/wp-content/uploads/2019/04/hipwee-43049075_726933011003614_6004018620840017920_n-640x800.jpg" fluid alt="Fluid image" class="banner"></b-img>
+                        <b-img src="https://1.bp.blogspot.com/-8B9CWsFeEW8/XRfuWcursHI/AAAAAAAAD0M/5vmxwCotz6cE_41LL2OseyFP7RmKs4KbQCLcBGAs/s640/foto-keluarga-studio-berdiri-sandar-background-abu-keren.jpg" fluid alt="Fluid image" class="banner"></b-img>
+                        <b-img src="https://isellerdotblog.files.wordpress.com/2020/06/adult-bag-beauty-model-blue-background-1126993.jpg?w=1024" fluid alt="Fluid image" class="banner"></b-img>
+                </carousel>
+            </div>
+            <div class="col-12 mt-5">
+                <h2 class="text-left text-black">Category</h2>
+                <p class="text-muted text-left">what are you currently looking for</p>
+            </div>
+            <div class="col-12 mt-1">
+                <carousel>
+                        <div class="image bg-primary"></div>
+                        <div class="image bg-warning"></div>
+                        <div class="image bg-success"></div>
+                        <div class="image bg-danger"></div>
+                        <div class="image bg-info"></div>
+                </carousel>
+            </div>
+            <div class="col-12 mt-5">
+                <h2 class="text-left text-black">Now</h2>
+                <p class="text-muted text-left">you've never seen it before</p>
+            </div>
+            <div class="col-12 mt-1 row">
+            <b-card-group>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://ae01.alicdn.com/kf/HTB1uKsVRFXXXXaLXVXXq6xXFXXXo/Blazer-pria-pakaian-resmi-terbaru-desain-mantel-celana-tuxedo-jas-pria-kostum-celana-pernikahan-pernikahan-setelan.jpg"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://sc02.alicdn.com/kf/HTB1M.dQNXXXXXbkXXXX760XFXXX7.png_350x350.png"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://ecs7.tokopedia.net/img/cache/700/product-1/2020/7/21/108511810/108511810_287c6191-adac-400b-b67e-444a416b58e0_700_700"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://ae01.alicdn.com/kf/HTB1_61aJpXXXXa_XFXXq6xXFXXXp/2015-baru-musim-gugur-busana-pria-baju-Slim-fit-kemeja-Warna-murni-kemeja-tuxedo-Lengan-panjang.jpg"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR6p7Ql6WkyWb1_sIp6NZnChtz2VBFLiHjQNw&usqp=CAU"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuK_2y1pPpJIvia5fnQ1AZmAEz9FQtPV4yoA&usqp=CAU"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Blezer Girl"
+                        img-src="https://img-z.okeinfo.net/okz/500/library/images/2020/08/04/fagdmi2iwqw7lkncs5q7_11948.jpg"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon><b-icon icon="star-fill"></b-icon>(1000)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://www.raramart.com/assets/uploads/products/2020-01-21-05-03-43-1579583023-nnnn.JPEG"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-12 mt-5">
+                <h2 class="text-left text-black">Popular</h2>
+                <p class="text-muted text-left">find clothes that are trending recently</p>
+            </div>
+            <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://sc02.alicdn.com/kf/HTB1M.dQNXXXXXbkXXXX760XFXXX7.png_350x350.png"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://ecs7.tokopedia.net/img/cache/700/product-1/2020/7/21/108511810/108511810_287c6191-adac-400b-b67e-444a416b58e0_700_700"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://ae01.alicdn.com/kf/HTB1_61aJpXXXXa_XFXXq6xXFXXXp/2015-baru-musim-gugur-busana-pria-baju-Slim-fit-kemeja-Warna-murni-kemeja-tuxedo-Lengan-panjang.jpg"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR6p7Ql6WkyWb1_sIp6NZnChtz2VBFLiHjQNw&usqp=CAU"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuK_2y1pPpJIvia5fnQ1AZmAEz9FQtPV4yoA&usqp=CAU"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Blezer Girl"
+                        img-src="https://img-z.okeinfo.net/okz/500/library/images/2020/08/04/fagdmi2iwqw7lkncs5q7_11948.jpg"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon><b-icon icon="star-fill"></b-icon>(1000)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+                <div class="col-3 text-left">
+                    <b-card
+                        title="Men Formal Suit Premium Quality"
+                        img-src="https://www.raramart.com/assets/uploads/products/2020-01-21-05-03-43-1579583023-nnnn.JPEG"
+                        img-alt="Image"
+                        img-top
+                        tag="article"
+                        style="max-width: 20rem;"
+                        class="mb-2"
+                    >
+                        <b-card-text>
+                        <span class=" text-danger font-weight-bold">$ 40.0</span><br>
+                        <span class="text-muted">Zalora Cloth</span><br>
+                        <span class="text-muted"><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill" variant="warning"></b-icon><b-icon icon="star-fill"></b-icon>(10)</span>
+                        </b-card-text>
+                    </b-card>
+                </div>
+             </b-card-group>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import carousel from 'vue-owl-carousel2'
+
+export default {
+  components: { carousel }
+}
+</script>
+
+<style scoped>
+.banner {
+    width: 456px;
+    height: 180px;
+    object-fit: cover;
+}
+.image {
+    width: 206px;
+    height: 220px;
+}
+.text {
+    font-size: 45px;
+}
+.overflow {
+    width:200px;
+    height:100px;
+    overflow-y:hidden;
+    overflow-x:scroll;
+}
+</style>

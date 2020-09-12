d466be22bf0f800aab78f17689a692270b4524f8
Deni Irawan Nugraha <60280044+deni-irawan40563@users.noreply.github.com>
2020-09-12 07:59:16 +0700

    split CardChart, CheckProductOnChart to atom component

---
+++ src/components/atoms/CardChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-12 mt-3 shadow-sm">
+        <div class="clearfix row">
+            <b-form-checkbox size="lg" class="col-1 mt-4 ml-3"></b-form-checkbox>
+            <b-img left src="https://picsum.photos/125/125/?image=58" alt="Left image" class="col-2"></b-img>
+            <div class="col-4">
+                <h4 class="text-left">Men's Formal Suit</h4>
+                <p class="text-left text-muted">Zalora Cloth</p>
+            </div>
+            <div class="col-3 row mt-3 text-center">
+                <div class="col-4">
+                    <b-button pill class="bg-grey shadow-sm">-</b-button>
+                </div>
+                <p class="col-4 mt-1"> 1 </p>
+                <div class="col-4">
+                    <b-button pill class="bg-white text-dark shadow-sm"> + </b-button>
+                </div>
+            </div>
+            <h5 class="col-2 mt-4 text-right">$ 20.0</h5>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>

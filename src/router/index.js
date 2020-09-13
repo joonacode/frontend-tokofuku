59a2c7db8767eb599f1f5dea8e5ec0f3e0d2c63b
andriabakti <andr.bkti@gmail.com>
2020-09-13 15:52:48 +0700

    checkout page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Auth/login/Login.vue'
-import Register from '../views/Auth/register/Register.vue'
+import Register from '@/views/Auth/register/Register.vue'
+import Checkout from '@/views/Checkout/Checkout.vue'
 
 Vue.use(VueRouter)
 
@@ -29,6 +30,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
   }
 ]
 

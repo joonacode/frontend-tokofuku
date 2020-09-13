c5bd1dbcef53c1f9bb6250c355ac32ed5e28708b
Deni Irawan Nugraha <60280044+deni-irawan40563@users.noreply.github.com>
2020-09-13 12:49:58 +0700

    My Order in Store Alan

---
+++ src/components/molecules/MyOrderOnStore.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <b-col cols="12">
+        <h1 class="text-left mt-5 order">My Order</h1>
+    </b-col>
+    <b-col cols="12" class="border-bottom">
+        <div>
+            <b-nav>
+                <b-nav-item href="#1" class="link">All Items</b-nav-item>
+                <b-nav-item href="#2" class="link">Get paid</b-nav-item>
+                <b-nav-item href="#2" class="link">Processed</b-nav-item>
+                <b-nav-item href="#2" class="link">Sent</b-nav-item>
+                <b-nav-item href="#2" class="link">Completed</b-nav-item>
+                <b-nav-item href="#2" class="link">Order Cancel</b-nav-item>
+            </b-nav>
+        </div>
+    </b-col>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control rounded-pill mt-3 ml-3" type="search" placeholder="Search" aria-label="Search">
+    </form>
+    <img src="../../assets/cart.svg" class="image">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.image {
+    margin-top: 6rem;
+}
+</style>
